2ebdd8c9382064682cf3442d4eb59fa555b46924
Ismayil Namazov <n4mazov>
2025-03-05 15:13:45 +0400

    Task4 ucun kod yazdim

---
+++ Task4 code.cs
@@ -0,0 +1,21 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int Dozen = num / 10;
+            int Unit = num % 10;
+            if(Dozen > Unit)
+            {
+                Console.WriteLine("Onlug boyukdur");
+            }
+            else
+            {
+                Console.WriteLine("Teklik boyukdur");
+            }
+        }
+    }
+}
