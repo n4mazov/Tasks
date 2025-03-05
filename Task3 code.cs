d1169a29219af0ae6771cc483c396ce5213a9d54
Ismayil Namazov <n4mazov>
2025-03-05 15:03:13 +0400

    Task3 kodunu yazdim

---
+++ Task3 code.cs
@@ -0,0 +1,22 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your salary:");
+            int salary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your kredit:");
+            int kredit = Convert.ToInt32(Console.ReadLine());
+            int yearSalary = salary * 12;
+            if (kredit > yearSalary * 0.6)
+            {
+                Console.WriteLine("Kredit goture bilersiniz");
+            }
+            else
+            {
+                Console.WriteLine("Kredit goture bilmesiniz");
+            }
+        }
+    }
+}
