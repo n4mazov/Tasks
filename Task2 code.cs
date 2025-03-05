5994724777d1770d97c1c2c4e2c4e5958c64a262
Ismayil Namazov <n4mazov>
2025-03-05 13:53:00 +0400

    Task2-nin kodunu yazdim

---
+++ Task2 code.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1%2 == 0 && num2%2==0)
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else
+            {
+                Console.WriteLine("Daxil olunan ededler cut olmalidir!");
+            }
+        }
+    }
+}
