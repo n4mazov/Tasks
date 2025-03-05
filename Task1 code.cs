108f3b98a492634446bf31eb2e39e26af2d5e8bb
Ismayil Namazov <n4mazov>
2025-03-05 13:34:05 +0400

    Task1 ucun kod yazdim

---
+++ Task1 code.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Specialized;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        #region Task1
+        {
+            Console.WriteLine("Enter your mark:");
+            int mark = Convert.ToInt32(Console.ReadLine());
+            if (mark > 90)
+            {
+                Console.WriteLine("Your mark is: A");
+            }
+            else if (mark > 80)
+            {
+                Console.WriteLine("Your mark is: B");
+            }
+            else if (mark > 70)
+            {
+                Console.WriteLine("Your mark is: C");
+            }
+            else if (mark > 61)
+            {
+                Console.WriteLine("Your mark is: D");
+            }
+            else if (mark < 61)
+            {
+                Console.WriteLine("Siz kesilmisiniz");
+            }
+            
+        }
+    }
+}
+#endregion
