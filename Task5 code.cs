973e1e83eee308455dd240849bf564daa3807a84
Ismayil Namazov <n4mazov>
2025-03-06 12:23:10 +0400

    Task5 ucun kod yazdim

---
+++ Task5 code.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your length:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your width:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int S = a * b;
+            int P = 2 * (a + b);
+            Console.WriteLine("Sahe beraberdir: " + S);
+            Console.WriteLine("Perimetr beraberdir: " + P);
+        }
+    }
+}
